2c86cfd623f24d13eeb1ca65a797b89b2fca8138
Nguyen Tran Thien Toan <thientoancubi@gmail.com>
2022-03-30 14:38:21 +0700

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Toan Thien on 30/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
